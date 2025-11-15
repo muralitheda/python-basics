750090ce78bb682310440b1a06c9bce7d01ec01a
muralitheda <murali.balasubramaniam@outlook.com>
2025-11-15 10:02:01 +0530

    latest changes

--- python-practice-scenarios-code.py
+++ python-practice-scenarios-code.py
@@ -3,6 +3,7 @@
 #########################################
 ## 🎯 Variables
 #########################################
+
 #1. Create two variables `x = 100` and `y = 10`, find their multiplication and division, and store the results in variables `z` and `z1`.
 x = 100
 y = 10
@@ -10,20 +11,179 @@ z = x * y
 z1 = x / y
 print("type(z):",type(z), "z:",z)
 print("type(z1):", type(z1), "z1:", z1)
+
 """
 type(z): <class 'int'> z: 1000
 type(z1): <class 'float'> z1: 10.0
 """
 
 #2. Create a variable `a = 2000`, divide `a` by `y` (from step 1), and reassign `a` with the divided result.
+print()
+
+a = 2000
+a = a / y
+print("type(a):",type(a),"a:",a)
+
+"""
+type(a): <class 'float'> a: 200.0
+"""
+
 #3. Prove Python is dynamically typed by creating `x: int = 100`, then assigning `y = str(x)` and printing the types of `x` and `y`.
+print()
+
+x: int = 100
+y = str(x)
+print("type(x):",type(x))
+print("type(y):",type(y))
+
+"""
+type(x): <class 'int'>
+type(y): <class 'str'>
+"""
+
 #4. Demonstrate Python’s dynamic inference feature with an example showing automatic datatype detection.
+print()
+
+code = 100
+print("code=",code)
+print("type(code)=",type(code))
+
+code = "Hundred"
+print("code=",code)
+print("type(code)=",type(code))
+
+code = 100/100
+print("code=",code)
+print("type(code)=",type(code))
+
+code = [1,2,3,4]
+print("code=",code)
+print("type(code)=",type(code))
+
+code = (1,2,3,4)
+print("code=",code)
+print("type(code)=",type(code))
+
+code = {1,2,3,4}
+print("code=",code)
+print("type(code)=",type(code))
+
+code = {"empid":1,"name":"jack"}
+print("code=",code)
+print("type(code)=",type(code))
+
+code = True
+print("code=",code)
+print("type(code)=",type(code))
+
+"""
+code= 100
+type(code)= <class 'int'>
+
+code= Hundred
+type(code)= <class 'str'>
+
+code= 1.0
+type(code)= <class 'float'>
+
+code= [1, 2, 3, 4]
+type(code)= <class 'list'>
+
+code= (1, 2, 3, 4)
+type(code)= <class 'tuple'>
+
+code= {1, 2, 3, 4}
+type(code)= <class 'set'>
+
+code= {'empid': 1, 'name': 'jack'}
+type(code)= <class 'dict'>
+
+code= True
+type(code)= <class 'bool'>
+"""
+
 #5. Prove Python is a strongly typed language using incompatible type operations.
+print()
+
+name = "Alan Harpor"
+age = 40
+
+print("name=",name,"type(name)=",type(name))
+print("age=",age,"type(age)=",type(age))
+
+#concatenate string + number is not possible
+#concate_name_age = name + age # strongly typed
+
+"""
+name= Alan Harpor type(name)= <class 'str'>
+age= 40 type(age)= <class 'int'>
+TypeError: can only concatenate str (not "int") to str
+"""
+
 #6. Create variables `a, b, c, d` assigned with values `10, 20, 30, 40` respectively.
+print()
+
+a = 10
+b = 20
+c = 30
+d = 40
+
+print(f"a={a} b={b} c={c} d={d}")
+
+# OR
+
+a,b,c,d = 10,20,30,40
+print(f"a={a} b={b} c={c} d={d}")
+
 #7. Prove that Python variable names are case-sensitive.
+print()
+
+name = "Alan Harpor"
+Name = "alan harpor"
+
+print(f"name={name} | Name={Name}")
+
+"""
+name=Alan Harpor Name=alan harpor
+"""
+
 #8. Demonstrate that variable names cannot start with numbers or contain special characters (except `_`).
+print()
+
+_number = 100  # valid
+number_ = 100  # valid
+"""
+1number = 100  # not valid
+$number = 100  # not valid
+num$ber = 100  # not valid
+"""
+
 #9. Show examples of using single, double, and triple quotes in Python.
+print()
+
+name = 'I cant wait'
+print(name)
+
+name = "I can't wait"
+print(name)
+
+name = """I 
+can't 
+wait
+"""
+print(name)
+
+"""
+I cant wait
+I can't wait
+I 
+can't 
+wait
+"""
+
 #10. Demonstrate examples using arithmetic, assignment, comparison, relational, and logical operators.
+
+
 
 #########################################
 ## 🎯 Conditional Structures
